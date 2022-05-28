3b11686f5108d331faa4f290b450ae1841886ae0
Marc Schiereck <mdreem@fastmail.fm>
2022-05-28 18:21:22 +0200

    add tests for provider data

--- endpoints/cache_test.go
+++ endpoints/cache_test.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"github.com/mdreem/s3_terraform_registry/s3"
 	"github.com/mdreem/s3_terraform_registry/schema"
+	"io"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -25,14 +27,35 @@ func defaultBucketContent() []string {
 
 type TestBucket struct {
 	entries []string
+	objects map[string]s3.BucketObject
 }
 
 func NewTestBucket(entries []string) TestBucket {
 	return TestBucket{entries: entries}
 }
 
+func NewTestBucketWithObjects(entries []string, objects map[string]s3.BucketObject) TestBucket {
+	return TestBucket{entries: entries, objects: objects}
+}
+
 func (bucket TestBucket) ListObjects() ([]string, error) {
 	return bucket.entries, nil
+}
+
+func (bucket TestBucket) GetObject(key string) (s3.BucketObject, error) {
+	object, ok := bucket.objects[key]
+	if ok {
+		return object, nil
+	}
+
+	stringReader := strings.NewReader("Object Data for: " + key)
+	stringReadCloser := io.NopCloser(stringReader)
+
+	return s3.BucketObject{
+		Body:          stringReadCloser,
+		ContentLength: int64(len(key)),
+		ContentType:   "ContentType: " + key,
+	}, nil
 }
 
 func NewTestProviderData() TestProviderData {
